8f17b0ae4e882980a21f4a8e40b0c0ee330e0c0d
Kevin Dutzy <kddutz15@gmail.com>
2023-03-30 00:32:11 -0400

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
@@ -6,10 +6,24 @@ import QuestionList from "./QuestionList";
 function App() {
   const [page, setPage] = useState("List");
 
+
+  const [questions, setQuestions] = useState([])
+
+  function handleDelete(id) {
+  const filteredQuestions = questions.filter((q) => q.id !== id)
+  setQuestions(filteredQuestions)
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+    .then(res => res.json())
+    .then((data) => setQuestions(data))
+  }, [])
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm setQuestions={setQuestions} questions={questions} /> : <QuestionList questions={questions} setQuestions={setQuestions} onDelete={handleDelete}/>}
     </main>
   );
 }
